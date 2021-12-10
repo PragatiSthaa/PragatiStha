5fe99f3df8b1497457d6ad8c3e275c26d234d7c9
Suprav_Stha <supravstha41@gmail.com>
2021-12-10 22:21:14 +0545

    Initial commit

---
+++ src/Assignment1Qno4.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Assignment1Qno4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number: ");
+        int n = sc.nextInt();
+        System.out.println("the integer are: ");
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i);
+        }
+        System.out.println("the sum of number is");
+        for (int i = 0; i < n; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
---
+++ src/Assignment1Qno5d.java
@@ -0,0 +1,13 @@
+public class Assignment1Qno5d {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = n; i > 0; i--) {
+            for (int b = i; b < n; b++)
+                System.out.print("  ");
+            for (int a = 2 * i; a > 1; a--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
